4309ddd25a90c17ca0439ea9951cc7211835998c
ritikakathare <ritikakathare15@gmail.com>
2025-11-28 03:53:53 +0530

    Update array.py

--- array.py
+++ array.py
@@ -1,16 +1,12 @@
 import sys
-
-# Check if scores are given through command-line
 if len(sys.argv) > 1:
     scores = [int(x) for x in sys.argv[1:]]
 else:
-    scores = [50, 60, 70, 80, 90]   # Default scores
-
+    scores = [50, 60, 70, 80, 90]  
 total = sum(scores)
 average = total / len(scores)
 maximum = max(scores)
 minimum = min(scores)
-
 print("\n--- LOCAL BRANCH OUTPUT ---")
 print("Scores:", scores)
 print("Total Score:", total)
