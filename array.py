d0c340d4883c7dd9dac5420a8a3e50b88f18e871
ritikakathare <ritikakathare15@gmail.com>
2025-11-28 03:35:05 +0530

    save

---
+++ array.py
@@ -0,0 +1,11 @@
+import sys
+if len(sys.argv) > 1:
+    scores = [int(x) for x in sys.argv[1:]]
+else:
+    scores = [50, 60, 70, 80, 90] 
+total = sum(scores)
+average = total / len(scores)
+print("\n--- MAIN BRANCH OUTPUT ---")
+print("Scores:", scores)
+print("Total Score:", total)
+print("Average Score:", average)
